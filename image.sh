549d6135b7e1a520fe4e886cca24b8d8cf7a3908
Florian Fusseder <fussederflorian@gmail.com>
2022-04-29 15:30:21 +0200

    Setup frontend

---
+++ image.sh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+
+if [ "$1" = "backend" ]; then
+  build_name="wordle-backend-ff"
+  folder_name="backend"
+elif [ "$1" = "frontend" ]; then
+  build_name="wordle-frontend-ff"
+  folder_name="frontend"
+else
+  echo "No known service with name '${1}', exiting..."
+  return 0
+fi
+
+echo "release $build_name"
+
+docker build -t wordle-"${folder_name}" -f "${folder_name}"/Dockerfile .
+docker tag wordle-"${folder_name}" registry.heroku.com/"${build_name}"/web
+if [ "$2" != "--release" ]; then
+  echo "Skip release, exiting..."
+  return 0
+fi
+docker push registry.heroku.com/"${build_name}"/web
+sha=$(docker inspect registry.heroku.com/"${build_name}"/web --format={{.Id}})
+curl --netrc -X PATCH https://api.heroku.com/apps/"${build_name}"/formation \
+  -d '{
+        "updates": [
+          {
+            "type": "web",
+            "docker_image": "'"${sha}"'"
+          }
+        ]
+      }' \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/vnd.heroku+json; version=3.docker-releases"
